79e4f7dec4fb5e0431a4b70ef161f2675ea870ad
gvdev666 <gvdev666@gmail.com>
2024-02-29 22:12:30 -0800

    correcciones

--- Model/UsuarioModel.php
+++ Model/UsuarioModel.php
@@ -1,24 +1,28 @@
 <?php
 
-class UsuarioModel {
+class UsuarioModel
+{
     private $conn;
 
-    public function __construct($conn) {
+    public function __construct($conn)
+    {
         $this->conn = $conn;
     }
 
-    public function registrarUsuario($nombre, $genero, $usuario, $contrasena) {
+    public function registrarUsuario($nombre, $genero, $usuario, $contrasena)
+    {
         $contrasenaHash = password_hash($contrasena, PASSWORD_DEFAULT);
 
         $sql = "INSERT INTO entrenadores (nombre, genero, usuario, contrasena) VALUES (?, ?, ?, ?)";
         $stmt = $this->conn->prepare($sql);
+
+        // Nota: El orden de los parámetros en bind_param debe coincidir con el orden en la consulta SQL
         $stmt->bind_param("ssss", $nombre, $genero, $usuario, $contrasenaHash);
 
         if ($stmt->execute()) {
             return true;
         } else {
-            return false;
+            throw new Exception("Error al registrar el usuario: " . $stmt->error);
         }
     }
 }
-?>
